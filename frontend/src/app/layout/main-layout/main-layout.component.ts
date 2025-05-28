d6125783cf5328217eb526980104c296806e021b
Douaa <douaasekal@gmail.com>
2025-05-28 13:48:31 +0100

    Add 1st version of sidebar, navbar, footer, main-layout

--- frontend/src/app/layout/main-layout/main-layout.component.ts
+++ frontend/src/app/layout/main-layout/main-layout.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { NavbarComponent } from '../navbar/navbar.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
+    ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
