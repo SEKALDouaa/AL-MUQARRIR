4ea5489b0741e5f46764febbe5f9db6c45bcf1a1
Douaa <douaasekal@gmail.com>
2025-05-29 19:51:40 +0100

    Fix sidebar errors and modify navbar

--- frontend/src/app/layout/navbar/navbar.component.ts
+++ frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -9,6 +10,26 @@ import { CommonModule } from '@angular/common';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  searchExpanded = false;
+  currentLang = 'FR'
+
+  toggleSearch(event: Event): void {
+    this.searchExpanded = !this.searchExpanded;
+    event.stopPropagation();
+  }
+
+  toggleLang() {
+    this.currentLang = this.currentLang === 'FR' ? 'AR' : 'FR';
+    // Optional: call a service to update app language here
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event): void {
+    if (this.searchExpanded) {
+      this.searchExpanded = false;
+    }
+  }
+
   theme = {
     navbar: '#333333'
   };
