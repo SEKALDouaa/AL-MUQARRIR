fd8be47ec67c1153894f8fd08622661dbb444a86
Douaa <douaasekal@gmail.com>
2025-05-31 17:51:09 +0100

    Fix __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     ma.init_app(app)
     cors.init_app(app)
 
-    app.register_blueprint(user_bp, url_prefix='/api/users')
+    app.register_blueprint(user_bp, url_prefix='/api')
 
     with app.app_context():
         db.create_all()
